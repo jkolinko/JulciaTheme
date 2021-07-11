c6a5a778a96f6fc65f64b7ae4437e5f3f104934b
Julia Kolinko <j.kolinko@auroracreation.com>
2021-07-11 21:15:18 +0200

    Theme files added

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Julcia/theme', __DIR__);
