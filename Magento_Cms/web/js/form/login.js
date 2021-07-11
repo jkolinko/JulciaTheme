27bf6e28a6415bb766aa983f370afa1177608193
Julia Kolinko <j.kolinko@auroracreation.com>
2021-07-11 23:43:09 +0200

    Knockout login and register templates added

---
+++ Magento_Cms/web/js/form/login.js
@@ -0,0 +1,9 @@
+define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
+    'use strict';
+
+    return Component.extend({
+        initialize: function () {
+            this._super();
+        }
+    });
+});
