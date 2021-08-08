3c197eed42bef7b72948706d540912de1ddd5aa9
Julia Kolinko <j.kolinko@auroracreation.com>
2021-08-08 19:32:49 +0200

    Login action added

--- Magento_Cms/web/js/form/login.js
+++ Magento_Cms/web/js/form/login.js
@@ -4,53 +4,96 @@ define([
     'ko',
     'Magento_Customer/js/customer-data',
     'mage/translate',
-    'Magento_Ui/js/view/messages',
+    'Magento_Customer/js/action/login',
     'mage/validation'
-], function($, Component, ko, customerData, $t) {
+], function($, Component, ko, customerData, $t, loginAction) {
     'use strict';
 
     return Component.extend({
         defaults: {
             tracks: {
-                username: true,
+                email: true,
                 password: true
             },
+            customerLoggedIn: false,
             loginFormSelector: '#blog-login-form',
-            loginValidationMessage: $t('Fill required fields.'),
-            loginErrorMessage: $t('Incorrect login data, please try again.')
+            loginValidationMessage: $t('Fill required fields.')
         },
 
         initialize: function() {
-            this._super();
+            this._super()
+                .observe(['email', 'password']);
 
             return this;
         },
 
-        getUsername: function() {
-            return this.username;
+        getEmail: function() {
+            return this.email();
         },
 
-        setUsername: function(value) {
-            this.username(value);
+        setEmail: function(value) {
+            this.email(value);
         },
 
         getPassword: function() {
-            return this.password;
+            return this.password();
         },
 
         setPassword: function(value) {
             this.password(value);
         },
 
-        validateForm: function(form) {
-            return form.validation() && form.validation('isValid');
+        validateForm: function() {
+            var self = this;
+            var loginForm = $(self.loginFormSelector);
+
+            return new Promise(function(resolve, reject) {
+                loginForm.validation();
+
+                if(loginForm.validation('isValid')) {
+                    resolve(true);
+                }
+
+                reject(false);
+            });
         },
 
         sendForm: function() {
             var self = this;
-            var loginForm = $(self.loginFormSelector);
 
-            self.validateForm(loginForm);
+            self.validateForm()
+                .then(function() {
+                    var user = {
+                        username: self.getEmail(),
+                        password: self.getPassword()
+                    };
+
+                    $.ajax({
+                        contentType: 'application/json',
+                        context: self.loginFormSelector,
+                        data: JSON.stringify(user),
+                        showLoader: true,
+                        type: 'POST',
+                        url: 'rest/default/V1/integration/customer/token',
+
+                        error: function() {
+                            self.setEmail('');
+                            self.setPassword('');
+                        },
+
+                        success: function() {
+                            loginAction(user);
+                        }
+                    })
+                })
+                .catch(function() {
+                    customerData.set('messages', {
+                        messages: [{
+                            type: 'error',
+                            text: self.loginValidationMessage
+                        }]
+                    })
+                });
         }
     })
 });
